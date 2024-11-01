b91c1ecd5dc3df5313e4711c942e247fe6b11230
OscarCodeLab <kolawoleopeyemioscar@gmail.com>
2024-11-01 18:43:01 +0100

    first commit

---
+++ gitdemo/src/main/java/demo/LoginPage.java
@@ -0,0 +1,28 @@
+package demo;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+
+public class LoginPage {
+
+	public WebDriver driver;
+@Test	
+public void logindetil() {
+	driver =new  ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.google.com/");
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	
+}
+
+@AfterTest
+public void teardown() {
+	driver.quit();
+}
+	
+}
