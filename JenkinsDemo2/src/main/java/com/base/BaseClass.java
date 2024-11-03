8fd64fe1ec96dddcaa0d26e2f20f7ef0a01aef31
OscarCodeLab <kolawoleopeyemioscar@gmail.com>
2024-11-03 06:02:01 +0100

    change the internetexplorer driver to edge driver

--- JenkinsDemo2/src/main/java/com/base/BaseClass.java
+++ JenkinsDemo2/src/main/java/com/base/BaseClass.java
@@ -2,8 +2,8 @@ package com.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -15,14 +15,11 @@ public class BaseClass {
 	@BeforeMethod
 	public void setup() {
 		if (System.getProperty("browser").equalsIgnoreCase("Chrome")) {
-			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
 		} else if (System.getProperty("browser").equalsIgnoreCase("Firefox")) {
-			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
 		} else if (System.getProperty("browser").equalsIgnoreCase("IE")) {
-			WebDriverManager.iedriver().setup();
-			driver = new InternetExplorerDriver();
+			driver = new EdgeDriver();
 		}
 		driver.manage().window().maximize();
 		driver.get(System.getProperty("url"));
